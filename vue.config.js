3c09be35c60cb484ef3f01b5d0f5aa3393ca679e
qianwenshengsec <qianwenshengsec@gmail.com>
2021-10-21 23:50:33 +0800

    xxx

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 module.exports = {
 
   configureWebpack: {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/nasa-photo-library/'
-    : '/',
+    publicPath: process.env.NODE_ENV === 'development' ? '/nasa-photo-library/' : '/',
     resolve: {
       alias: {
         'assets': '@/assets',
